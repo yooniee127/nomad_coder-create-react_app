f202c8cc4394ef5ab1870c925850bd1a89dbfa13
yooniee127 <62793049+yooniee127@users.noreply.github.com>
2023-01-31 18:42:19 +0900

    22/01/31_5.1_useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+// import Button from "./Button";
+// import styles from "./App.module.css"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0)
+  const onClick = () => setValue((prev) => prev + 1);
+  
+  console.log("I run all the time.");
+  useEffect(() => {
+    console.log("I run only once OR Call the API")
+  }, []);
+  
   return (
     <div>
-      <h1 className={styles.title}>Welcom back!</h1>
-      <Button text={"Continue"} />
-    </div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
+  </div>
   );
 }
 export default App;
